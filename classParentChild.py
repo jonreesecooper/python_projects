911d66bd6b27d12f2aa39b70ba0cad508a9d5120
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-10-02 14:53:19 -0400

    Add files via upload

---
+++ classParentChild.py
@@ -0,0 +1,15 @@
+class Musician:
+    ID = 0
+    knows_theory = True
+    performs_live = True
+    records_albums = False
+    genre_performed = ""
+
+class Guitarist(Musician):
+    guitar_type = ""
+    guitar_model = ""
+
+class Drummer(Musician):
+    drumkit_type = ""
+    drumkit_model = ""
+    
