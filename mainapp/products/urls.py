89b8368d41e85d926235a0309bc4dde93a80b6eb
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-10-14 18:17:21 -0400

    Add files via upload

---
+++ mainapp/products/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import include
+
+
+urlpatterns = [
+    path('admin_console', views.admin_console, name='admin_console'),
+    path('<int:pk>/details/', views.details, name='details'),
+    # the int:pk defines the variable we set in our details method
+]
